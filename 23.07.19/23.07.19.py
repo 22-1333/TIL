b00a8cde770df27d254a6347aec1768380b62a84
213 <epmdrpl213@gmail.com>
2023-07-19 17:57:14 +0900

    class

---
+++ 23.07.19/23.07.19.py
@@ -0,0 +1,150 @@
+# 1
+
+'''
+def get_sum(num1, num2):
+    return num1 + num2
+
+num1 = 5
+num2 = 3
+result = get_sum(num1, num2)
+
+print(result)
+
+# without parameter
+def get_sum_1():
+    num1 = 5
+    num2 = 3
+    return num1 + num2
+
+print(get_sum_1())
+
+# without return statement
+def get_sum_2(num1, num2):
+    print(num1 + num2)
+
+get_sum_2(num1, num2)
+'''
+
+# 2
+
+'''
+# positional argument
+def greet(name, age):
+    print(f"Hi, {name}! You are {age} years old.")
+
+greet("Alice", 25) # Hi, Alice! You are 25 years old.
+
+# default argument
+def greet(name, age = 25):
+    print(f"Hi, {name}! You are {age} years old.")
+
+greet("Alice") # Hi, Alice! You are 25 years old.
+
+# keyword arguments
+def greet(name, age):
+    print(f"Hi, {name}! You are {age} years old.")
+
+greet(name = "Alice", age = 25) # Hi, Alice! You are 25 years old.
+greet(age = 25, name = "Alice") # Hi, Alice! You are 25 years old.
+
+# arbitrary arguments
+def calculate_sum(*args):
+    print(args)
+    total = sum(args)
+    print(f"sum : {total}")
+
+calculate_sum(1, 2, 3, 4, 5)
+
+# arbitrary keyword arguments
+def print_info(**kwargs):
+    print(kwargs)
+
+print_info(name = "Eve", age = "30", height = 175.38)
+'''
+
+# 3
+
+'''
+z = 3 # global scope
+
+def outer():
+    x = 1 
+    def inner():
+        y = 2 # local scope
+        result = x + y # enclosed scope
+        # built-in scope -> len(), print(), input()...
+        print(result)
+    inner()
+
+outer()
+'''
+
+# 4
+
+'''
+a, b, c = 1, 2, 3
+
+def enclosed():
+    a, b, c = 4, 5, 6
+    def local(c):
+        print(a, b, c) # 4, 5, 500
+
+    local(500)
+    print(a, b, c) # 4, 5, 6
+
+enclosed()
+print(a, b, c) # 1, 2, 3
+
+# global
+
+a, b, c = 1, 2, 3
+
+def enclosed():
+    global a, b, c
+    a, b, c = 4, 5, 6
+    def local(c):
+        print(a, b, c) # 4, 5, 500
+
+    local(500)
+    print(a, b, c) # 4, 5, 6
+
+enclosed()
+print(a, b, c) # 4, 5, 6
+'''
+
+# 5
+
+'''
+def factorial(num):
+    # base case
+    if num == 0:
+        return 1
+
+    return num * factorial(num - 1) # toward base case
+
+n = int(input())
+print(factorial(n))
+'''
+
+# 6
+
+'''
+names = ["Kai", "Jane", "Bob"]
+ages = [30, 31, 27]
+cities = ["Seoul", "London", "Paris"]
+
+for name, age, city in zip(names, ages, cities):
+    print(f"my name is {name}, I am {age} years old and live in {city}.")
+'''
+
+# 7
+
+'''
+numbers = [1, 2, 3, 4, 5]
+
+print(list(map(lambda x: x ** 2, numbers)))
+'''
+
+numbers = [1, 2, 3, 4, 5]
+
+print(list(map(lambda x: x ** 2, numbers)))
